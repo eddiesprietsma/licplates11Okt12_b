94541e51ff48b4923684a8265404b5fb98f041be
Eddie Sprietsma <eddiesprietsma@hotmail.com>
2012-10-12 22:16:42 +0200

    License Plate framework

---
+++ C#/src/LicPlate/LicensePlateMatcher.cs
@@ -0,0 +1,233 @@
+﻿using System;
+
+namespace LicPlate
+{
+    public class LicensePlateMatcher
+    {
+        /*  Description:
+                Find the largest license plate in the image
+                    - Segment using ThresholdHSVchannels
+                    - Remove blobs which are not license plates
+            Input:
+	            //Original image
+	            RGB888Image plateImage	
+            Output:
+	            //Segmented license plate
+	            ref Int16Image binaryPlateImage
+            Return:	
+	            //License plate found?
+	            bool 
+         */	
+        public static bool FindPlate(RGB888Image plateImage, ref Int16Image binaryPlateImage)
+        {
+            //Constants
+            const int c_threshold_h_min = 21;
+            const int c_threshold_h_max = 70;
+            const int c_threshold_s_min = 100;
+            const int c_threshold_s_max = 255;
+            const int c_threshold_v_min = 100;
+            const int c_threshold_v_max = 255;
+            const int c_remove_blobs_min = 1;
+            const int c_remove_blobs_max = 500;
+            
+
+
+            //*******************************//
+            //** Exercise:                 **//
+            //**   adjust licenseplate     **//
+            //**   segmentation            **//
+            //*******************************//            
+        
+            //Find licenseplate
+            HSV888Image plateImageHSV = new HSV888Image();
+            //Convert to RGB to HSV
+            VisionLab.FastRGBToHSV(plateImage, plateImageHSV);
+
+            //Threshold HSV image
+            VisionLab.Threshold3Channels(plateImageHSV, binaryPlateImage, c_threshold_h_min, c_threshold_h_max, c_threshold_s_min, c_threshold_s_max, c_threshold_v_min, c_threshold_v_max);
+            
+            //Convert to a 32 bit format 
+            Int32Image binaryPlateImage32 = new Int32Image();
+            VisionLab.Convert(binaryPlateImage, binaryPlateImage32);
+
+            //Remove blobs with small areas
+            VisionLab.RemoveBlobs(binaryPlateImage32, Connected.EightConnected, BlobAnalyse.BA_Area, c_remove_blobs_min, c_remove_blobs_max);
+
+            //Remove border blobs
+            VisionLab.RemoveBorderBlobs(binaryPlateImage32, Connected.EightConnected, Border.AllBorders);
+
+            //Length Breath Ratio
+            VisionLab.RemoveBlobs(binaryPlateImage32, Connected.EightConnected, BlobAnalyse.BA_LengthBreadthRatio, 0, 2);
+            VisionLab.RemoveBlobs(binaryPlateImage32, Connected.EightConnected, BlobAnalyse.BA_LengthBreadthRatio, 7, 10);
+            
+            //Convert back to a 16 bit format
+            VisionLab.Convert(binaryPlateImage32, binaryPlateImage);
+
+            binaryPlateImage32.Dispose();
+            plateImageHSV.Dispose();
+
+            //Return true, if pixels found
+            return (VisionLab.SumIntPixels(binaryPlateImage) > 0);
+            //return VisionLab.LabelBlobs(binaryPlateImage, Connected.EightConnected) == 1;      
+        }
+
+        /*  Description:
+                Locates the characters of the license plate
+	                - Warp image (Rectify)
+	                - Segment characters
+	                - Remove blobs which are to small (Lines between characters)
+            Input:
+	            //Original image
+	            RGB888Image plateImage
+	            //Segmented license plate
+	            Int16Image binaryPlateImage
+            Output:
+	            //Image containing binary six characters	
+	            ref Int16Image binaryCharacterImage 
+            Return:
+	            //Function executed successfully
+	            bool
+        */
+        public static bool FindCharacters(RGB888Image plateImage, Int16Image binaryPlateImage, ref Int16Image binaryCharacterImage)
+        {
+            //Constants
+            const int c_height = 100;
+            const int c_width = 470;
+            const int c_remove_blobs_min = 1;
+            const int c_remove_blobs_max = 400;
+
+            XYCoord leftTop = new XYCoord();
+            XYCoord rightTop = new XYCoord();
+            XYCoord leftBottom = new XYCoord();
+            XYCoord rightBottom = new XYCoord();
+
+
+            //Find licenseplate
+            Int32Image binaryPlateImage32 = new Int32Image();
+            VisionLab.Convert(binaryPlateImage, binaryPlateImage32);
+            VisionLab.FindCornersRectangle(binaryPlateImage32, Connected.EightConnected, 0.5, Orientation.Landscape, leftTop, rightTop, leftBottom, rightBottom);
+            binaryPlateImage32.Dispose();
+
+            Int16Image plateImageGray = new Int16Image();
+
+            VisionLab.Convert(plateImage, plateImageGray);
+            binaryCharacterImage.Assign_Op(plateImageGray);
+            try
+            {
+                //Rectify plate
+                VisionLab.Warp(plateImageGray, binaryCharacterImage, TransformDirection.ForwardT, new Coord2D(leftTop), new Coord2D(rightTop), new Coord2D(leftBottom), new Coord2D(rightBottom), c_height, c_width, 0);
+            }
+            catch (Exception )
+            {
+                //Warp, 3 coords on one line
+                return false;
+            }
+            plateImageGray.Dispose();
+
+            //*******************************//
+            //** Exercise:                 **//
+            //**   adjust licenseplate     **//
+            //**   segmentation            **//
+            //*******************************//
+
+            //Find dark text on bright licenseplate using ThresholdISOData
+            VisionLab.ThresholdIsoData(binaryCharacterImage, ObjectBrightness.DarkObject);
+
+            //Convert to a 32 bit format 
+            Int32Image binaryCharacterImage32 = new Int32Image();
+            VisionLab.Convert(binaryCharacterImage, binaryCharacterImage32);
+            
+            //Remove blobs connected to the border
+            VisionLab.RemoveBorderBlobs(binaryCharacterImage32, Connected.EightConnected, Border.AllBorders);
+            //Remove small blobs
+            VisionLab.RemoveBlobs(binaryCharacterImage32, Connected.EightConnected, BlobAnalyse.BA_Area, c_remove_blobs_min, c_remove_blobs_max);
+
+            //Convert to a 16 bit format             
+            VisionLab.Convert(binaryCharacterImage32, binaryCharacterImage);
+
+            binaryCharacterImage32.Dispose();
+            leftTop.Dispose();
+            rightTop.Dispose();
+            leftBottom.Dispose();
+            rightBottom.Dispose();
+            GC.Collect();
+            return true;
+        }
+
+        /*
+            Description:
+                Read the license plate
+            Input:
+	            //Rectified license plate image containing six characters	
+	            Int16Image labeledRectifiedPlateImage
+	            BlobMatcher_Int16 matcher	//initialized blobmatcher
+	            ClassLexicon lexicon		//initialized lexicon
+            Output:
+	            //Result by the blob matcher
+	            ref LicensePlate result
+	            //Result by the lexicon
+	            ref LicensePlate lexiconResult
+            Return:
+	            //six characters found
+	        bool 
+        */
+        public static bool MatchPlate(Int16Image binaryCharacterImage, BlobMatcher_Int16 matcher, ClassLexicon lexicon, ref LicensePlate result, ref LicensePlate lexiconResult)
+        {
+            //Check if 6 characters/blobs have been found and label image.
+            if (VisionLab.LabelBlobs(binaryCharacterImage, Connected.EightConnected) != 6)
+                return false;
+
+            //Calculate dimensions and locations of all characters/blobs.
+            vector_BlobAnalyse ba_vec = new vector_BlobAnalyse();
+            ba_vec.Add(BlobAnalyse.BA_TopLeft);
+            ba_vec.Add(BlobAnalyse.BA_Height);
+            ba_vec.Add(BlobAnalyse.BA_Width);
+            vector_Blob returnBlobs = new vector_Blob();
+            VisionLab.BlobAnalysis(binaryCharacterImage, VisionLab.VectorToSet_BlobAnalyse(ba_vec), VisionLab.MaxPixel(binaryCharacterImage), returnBlobs, SortOrder.SortDown, BlobAnalyse.BA_TopLeft, UseXOrY.UseX);
+            ba_vec.Dispose();
+            Int16Image binaryCharacter = new Int16Image();
+
+            //Create data structure for lexicon.
+            vector_vector_LetterMatch match = new vector_vector_LetterMatch();
+            
+            //Process each character/blob.
+            foreach (Blob b in returnBlobs)
+            {
+                //Cut out character
+                VisionLab.ROI(binaryCharacterImage, binaryCharacter, b.TopLeft(), new HeightWidth(b.Height(), b.Width()));
+                //Convert ROI result to binary
+                VisionLab.ClipPixelValue(binaryCharacter, 0, 1);
+                //Calculate character's classification for all classes.
+                vector_PatternMatchResult returnMatches = new vector_PatternMatchResult();
+                float conf = matcher.AllMatches(binaryCharacter, (float)-0.5, (float)0.5, returnMatches);
+                float err = returnMatches[0].error;
+                int id = returnMatches[0].id;
+                string chr = matcher.PatternName(id);
+                
+                //Fill datastructure for lexicon.
+                match.Add(VisionLabEx.PatternMatchResultToLetterMatch(returnMatches));
+                
+                //Store best match in result
+                result.characters.Add(new LicenseCharacter(chr, err, conf));
+            }
+
+            //Validate match with lexicon.
+            vector_int bestWord = new vector_int();
+            lexiconResult.confidence = lexicon.FindBestWord(match, bestWord, Optimize.OptimizeForMinimum);
+            for (int i = 0; i < bestWord.Count; i++)
+            {
+                string character = matcher.PatternName(bestWord[i]);
+                //Store lexicon result
+                lexiconResult.characters.Add(new LicenseCharacter(character));
+            }
+
+            binaryCharacter.Dispose();
+            returnBlobs.Dispose();
+            match.Dispose();
+            bestWord.Dispose();
+            GC.Collect();
+            return true;
+        }
+    }
+}
+
---
+++ C#/src/LicPlate/VisionLabEx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace LicPlate {
+    public class VisionLabEx
+    {
+
+        [DllImport("msvcrt.dll", EntryPoint = "memcpy")]
+        public unsafe static extern void CopyMemory(IntPtr pDest, IntPtr pSrc, int length);
+
+        public static RGB888Image BitmapToJL(Bitmap bm)
+        {
+            RGB888Image vlimage;
+            BitmapData bmdata = bm.LockBits(new Rectangle(0, 0, bm.Width, bm.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            try
+            {
+                vlimage = new RGB888Image(bmdata.Height, bm.Width);
+                CopyMemory(vlimage.GetBufPtr(), bmdata.Scan0, bm.Width * bm.Height * 4 /*RGB8*/);
+            }
+            finally
+            {
+                bm.UnlockBits(bmdata);
+            }
+            return vlimage;
+        }
+
+        public static Bitmap JLToBitmap(Image img)
+        {
+            Bitmap bm = new Bitmap(img.GetWidth(), img.GetHeight(), PixelFormat.Format32bppRgb);
+            BitmapData bmdata = bm.LockBits(new Rectangle(0, 0, bm.Width, bm.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
+            try
+            {
+                RGB888Image vlimage = new RGB888Image();
+                VisionLab.Convert(img, vlimage);
+                CopyMemory(bmdata.Scan0, vlimage.GetBufPtr(), vlimage.GetWidth() * vlimage.GetHeight() * 4 /*RGB8*/);
+                vlimage.Dispose();
+            }
+            finally
+            {
+                bm.UnlockBits(bmdata);
+            }
+            return bm;
+        }
+
+        public static vector_LetterMatch PatternMatchResultToLetterMatch(vector_PatternMatchResult vpmr)
+        {
+            vector_LetterMatch ret = new vector_LetterMatch();
+            for (int i = 0; i < vpmr.Count; i++)
+            {
+                ret.Add(PatternMatchResultToLetterMatch(vpmr[i]));
+            }
+            return ret;
+        }
+        public static LetterMatch PatternMatchResultToLetterMatch(PatternMatchResult pmr)
+        {
+            return new LetterMatch(pmr.id, pmr.error);
+        }
+    }
+}
